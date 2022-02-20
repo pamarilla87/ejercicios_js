2831b6f100cd3e961412ef590f0a996c5d5d34ee
pamarilla <dl.neurus@gmail.com>
2022-02-19 21:57:15 -0300

    first commit

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -8,7 +8,7 @@ while (base != 0) {
     for (let i = 0; i < exp-1; i++) {
         aux = aux*base;
     }
-    alert("El resultado es:  " + aux)
+    alert("El resultado es: " + aux)
     base = parseInt(prompt("Vamos a calcular una potencia. Ingrese la base o 0 para salir"));
     exp= -1;
 }
